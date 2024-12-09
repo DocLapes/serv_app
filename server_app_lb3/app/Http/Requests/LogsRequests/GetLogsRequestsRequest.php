7164ec5373f466fc262eb48cc4867a374a0d0ccf
DocLapes <ev0202@mail.ru>
2024-12-09 14:12:36 +0500

    comm_6.2

--- server_app_lb3/app/Http/Requests/LogsRequests/GetLogsRequestsRequest.php
+++ server_app_lb3/app/Http/Requests/LogsRequests/GetLogsRequestsRequest.php
@@ -13,11 +13,11 @@ class GetLogsRequestsRequest extends FormRequest
     {
         return [
             'filters' => 'array',
-            'filters.*.key' => 'required|string|in:user_id,response_status,user_ip,user_agent,controller',
+            'filters.*.key' => 'required|string|in:user_id,response_status,user_ip,user_agent,controller,http_method',
             'filters.*.value' => 'required|string',
 
             'sortBy' => 'array',
-            'sortBy.*.key' => 'required|string|in:url,controller,controller_method,response_status,created_at',
+            'sortBy.*.key' => 'required|string|in:url,controller,controller_method,response_status,created_at,http_method',
             'sortBy.*.order' => 'required|string|in:asc,desc',
 
             'page' => 'integer|min:1',
