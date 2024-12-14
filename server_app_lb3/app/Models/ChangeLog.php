c741a11bf2821f4b95d6710ca7da18c5c63578cd
DocLapes <ev0202@mail.ru>
2024-12-15 00:27:09 +0500

    comm_10.2
    adding parsing and importing

--- server_app_lb3/app/Models/ChangeLog.php
+++ server_app_lb3/app/Models/ChangeLog.php
@@ -15,6 +15,7 @@ class ChangeLog extends Model
      * The attributes that are mass assignable.
      */
     protected $fillable = ['entity_name', 'entity_id', 'old_values', 'new_values', 'created_by'];
+    protected $hidden = ['created_by', 'created_at'];
 
     public function author() : BelongsTo
     {
@@ -55,56 +56,4 @@ class ChangeLog extends Model
             'created_by' => auth()->id(),
         ]);
     }
-    public static function restoreEntity(int $changeLogId): bool
-    {
-        // Получаем запись из журнала изменений
-        $changeLog = static::find($changeLogId);
-
-        if (!$changeLog) {
-            return false; // Запись не найдена
-        }
-
-        // Получаем класс сущности и идентификатор
-        $entityClass = $changeLog->entity_name;
-        $entityId = $changeLog->entity_id;
-
-        // Находим сущность
-        $entity = $entityClass::find($entityId);
-
-        if (!$entity) {
-            return false; // Сущность не найдена
-        }
-
-        // Восстанавливаем старые значения
-        
-        $oldValues = json_decode($changeLog->old_values, true);
-
-        $newValues1 = [];
-        foreach ($oldValues as $field => $value) {
-            $newValues1[$field] = $entity->$field; // Текущие значения сущности
-        }
-
-        foreach ($oldValues as $field => $value) {
-            $entity->$field = $value; // Восстанавливаем старое значение
-        }
-        // Сохраняем изменения
-        $entity->save();
-
-        $newValues = [];
-        foreach ($oldValues as $field => $value) {
-            $newValues[$field] = $entity->$field; // Текущие значения сущности
-        }
-
-        static::insert([
-            'entity_name' => $entityClass,
-            'entity_id' => $entityId,
-            'old_values' => json_encode($newValues1), // Текущие значения (старые значения)
-            'new_values' => json_encode($newValues), // Старые значения (новые после восстановления)
-            'created_by' => auth()->id(),
-        ]);
-
-        return true;
-    
-    }
 }
-
